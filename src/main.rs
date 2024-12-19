85b0a29163dbeece7e3bba74b40d33dda0e68593
SohamDesai1 <sohamdesai123@gmail.com>
2024-12-20 01:32:33 +0530

    feat: :zap: added loop for multiple analysis

--- src/main.rs
+++ src/main.rs
@@ -36,50 +36,76 @@ fn main() {
                 ("Qualifying", "Qualifying", "quali"),
                 ("Race", "R", "race"),
             ];
-            let sesh_names: Vec<_> = sessions.iter().map(|(sesh, _, _)| sesh).collect();
-            let select_sesh = Select::with_theme(&ColorfulTheme::default())
-                .with_prompt("Select a driver you want to analyze:")
-                .default(0)
-                .items(&sesh_names)
-                .interact()
-                .unwrap();
 
-            let (sesh_name, _, _) = sessions[select_sesh];
-            let (_, sesh, _) = sessions[select_sesh];
-            let (_, _, sesh_var_name) = sessions[select_sesh];
+            loop {
+                let mut sesh_names: Vec<_> = sessions.iter().map(|(sesh, _, _)| sesh).collect();
+                sesh_names.push(&"Cancel");
+                let select_sesh = Select::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Select a session you want to analyze:")
+                    .default(0)
+                    .items(&sesh_names)
+                    .interact()
+                    .unwrap();
 
-            add_cell(file_path_str, vec![format!("*{}*", sesh_name)], "markdown");
-            add_cell(
-                file_path_str,
-                vec![
-                    format!(
-                        "{} = f1.get_session(2024, \"Qatar\", \"{}\")\n",
-                        sesh_var_name, sesh
-                    ),
-                    format!("{}.load()", sesh_var_name),
-                ],
-                "code",
-            );
-            add_cell(
-                file_path_str,
-                vec![format!("{}.session_info", sesh_var_name)],
-                "code",
-            );
-            add_cell(
+                if select_sesh == sesh_names.len() - 1 {
+                    println!("Exiting session analysis.");
+                    break;
+                }
+                let (sesh_name, _, _) = sessions[select_sesh];
+                let (_, sesh, _) = sessions[select_sesh];
+                let (_, _, sesh_var_name) = sessions[select_sesh];
+
+                add_cell(file_path_str, vec![format!("*{}*", sesh_name)], "markdown");
+                add_cell(
+                    file_path_str,
+                    vec![
+                        format!(
+                            "{} = f1.get_session(2024, \"Qatar\", \"{}\")\n",
+                            sesh_var_name, sesh
+                        ),
+                        format!("{}.load()", sesh_var_name),
+                    ],
+                    "code",
+                );
+                add_cell(
+                    file_path_str,
+                    vec![format!("{}.session_info", sesh_var_name)],
+                    "code",
+                );
+                add_cell(
                         file_path_str,
                         vec![format!("{}.results.loc[\n",sesh_var_name),
             "    :, [\"Abbreviation\", \"TeamName\", \"GridPosition\", \"Position\", \"Time\", \"Status\"]\n".to_string(),
             "]".to_string()],"code"
                     );
-            add_cell(
-                file_path_str,
-                vec![format!("{}.laps", sesh_var_name)],
-                "code",
-            );
-            driver_analysis(file_path_str, sesh_var_name);
-            run_notebook(file_path_str, python_dir.clone());
-            driver_analysis(file_path_str, sesh_var_name);
-            run_notebook(file_path_str, python_dir);
+                add_cell(
+                    file_path_str,
+                    vec![format!("{}.laps", sesh_var_name)],
+                    "code",
+                );
+
+                let options = vec!["Yes", "No"];
+                loop {
+                    let do_analysis = Select::with_theme(&ColorfulTheme::default())
+                        .with_prompt("Analyze a driver?")
+                        .default(0)
+                        .items(&options)
+                        .interact()
+                        .unwrap();
+                    match do_analysis {
+                        0 => {
+                            driver_analysis(file_path_str, sesh_var_name);
+                            run_notebook(file_path_str, python_dir.clone());
+                            println!("Driver Analysis complete.");
+                        }
+                        1 => {
+                            println!("Exit Driver Analysis.");
+                            break;
+                        }
+                        _ => (),
+                    }
+                }
+            }
         } else {
             println!("Failed to convert the file path to a valid string.");
         }
